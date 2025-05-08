f4f934b5a0226cd62c0b7a65aa7b01330d31a33d
Sunil Poudel <suneelpdl90@gmail.com>
2025-05-08 15:57:01 +0545

    create entity Notes

---
+++ src/main/java/org/sunil_spring_demo/notes_web/entity/Notes.java
@@ -0,0 +1,63 @@
+package org.sunil_spring_demo.notes_web.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+public class Notes {
+    //fields
+    @Id
+    private int id;
+
+    private String date;
+
+    private String mood;
+
+    private String text;
+
+    //constructors
+
+    public Notes() {
+    }
+
+    public Notes(String date, String mood, String text) {
+        this.date = date;
+        this.mood = mood;
+        this.text = text;
+    }
+
+    //getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
